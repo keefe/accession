88cae16534375e85cff055d5e361022dab1a4aba
keefe <keefe@categorize.us>
2015-08-23 16:10:22 -0700

    loading very rough data from HN and posting to categorizeus

--- src/main/java/us/categorize/accession/App.java
+++ src/main/java/us/categorize/accession/App.java
@@ -1,6 +1,7 @@
 package us.categorize.accession;
 
 import java.io.IOException;
+import java.net.HttpCookie;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
@@ -15,10 +16,17 @@ import us.categorize.accession.proto.apis.HackerNews;
  */
 public class App 
 {
+	public static HttpCookie cookie;
     public static void main( String[] args ) throws JsonParseException, JsonMappingException, InterruptedException, ExecutionException, TimeoutException, IOException
     {
+    	//to retrieve this value, loging to the server locally and check the logs
         System.out.println( "Hello World!" );
+        cookie = new HttpCookie("JSESSIONID", "2F6124812BE33A1D7D7D01E57FD8EE54");
+//        cookie.setDomain("");
+//        cookie.setMaxAge(123);
         HackerNews news = new HackerNews();
         news.readTopStories();
     }
+    
+   
 }
--- src/main/java/us/categorize/accession/proto/apis/HackerNews.java
+++ src/main/java/us/categorize/accession/proto/apis/HackerNews.java
@@ -1,29 +1,41 @@
 package us.categorize.accession.proto.apis;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
 import org.eclipse.jetty.client.HttpClient;
 import org.eclipse.jetty.client.api.ContentResponse;
+import org.eclipse.jetty.client.util.StringContentProvider;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import us.categorize.accession.App;
+
 public class HackerNews {
 	private String apiBase = "https://hacker-news.firebaseio.com/v0/";
 	private HttpClient client;
+	private HttpClient categorizeUs;
+
 	private ObjectMapper mapper;
+	private static final String SESSIONID="608AFEF6FA27E13C22DEEB484FBAA585";
 
 	public HackerNews(){
 		mapper = new ObjectMapper();
 		SslContextFactory sslContextFactory = new SslContextFactory();
 
 		client = new HttpClient(sslContextFactory);
+		
+		categorizeUs = new HttpClient(sslContextFactory);
 		try {
 			client.start();
+			categorizeUs.start();
+			categorizeUs.getCookieStore().add(URI.create("http://categorize.us:8080"), App.cookie);
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -41,7 +53,11 @@ public class HackerNews {
 		String top[] = topStories();
 		for(String story : top){
 			String response = client.GET(apiBase + "item/" + story + ".json").getContentAsString();
-			System.out.println(response);
+			response.replaceAll("\"text\"", "\"body\"");
+			ContentResponse categorizeUsResponse =categorizeUs.POST("http://categorize.us:8080/thread")
+											.content(new StringContentProvider(response), "application/json")
+											.send();
+			System.out.println(response+ " ||| " + categorizeUsResponse.getContentAsString());
 		}
 	}
 	private void get(String loc) throws InterruptedException, ExecutionException, TimeoutException, JsonParseException, JsonMappingException, IOException{
